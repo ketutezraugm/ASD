d303cf9ec35ce03abc56210c57ffb2afe7bd3644
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2025-06-06 22:37:40 +0700

    Update AdjencyMatrix.cpp

--- Quiz Tutor 2/Chapter 2/Quiz_Answer/AdjencyMatrix.cpp
+++ Quiz Tutor 2/Chapter 2/Quiz_Answer/AdjencyMatrix.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const double INF = 1e9; // Representing infinity
+const double INF = 1e9;
 
+// add edge
 void addEdge(vector<vector<double>> &mat, int i, int j, double distance) {
     mat[i][j] = distance;
 }
 
+// print adjacency matrix
 void displayMatrix(vector<vector<double>> &mat) {
     int V = mat.size();
 
@@ -26,29 +28,29 @@ int main() {
     int V = 5;
     vector<vector<double>> mat(V, vector<double>(V, INF));
 
-    // Set diagonal to 0
+    // set diagonal to 0
     for (int i = 0; i < V; i++)
         mat[i][i] = 0.0;
 
-    // Add edges with weights
+    // add edges with weights
     addEdge(mat, 0, 1, 4.4); // Home -> GSP
     addEdge(mat, 0, 2, 4.4); // Home -> RSGM
 
-    addEdge(mat, 1, 0, 4.4);
-    addEdge(mat, 1, 2, 2.1);
-    addEdge(mat, 1, 3, 0.5);
-    addEdge(mat, 1, 4, 2.1);
+    addEdge(mat, 1, 0, 4.4); // GSP -> Home
+    addEdge(mat, 1, 2, 2.1); // GSP -> RSGM
+    addEdge(mat, 1, 3, 0.5); // GSP -> Balairung UGM
+    addEdge(mat, 1, 4, 2.1); // GSP -> Faculty of MIPA
 
-    addEdge(mat, 2, 0, 4.4);
-    addEdge(mat, 2, 1, 2.1);
-    addEdge(mat, 2, 4, 1.5);
+    addEdge(mat, 2, 0, 4.4); // RSGM -> Home
+    addEdge(mat, 2, 1, 2.1); // RSGM -> GSP
+    addEdge(mat, 2, 4, 1.5); // RSGM -> Faculty of MIPA
 
-    addEdge(mat, 3, 1, 0.5);
-    addEdge(mat, 3, 4, 1.9);
+    addEdge(mat, 3, 1, 0.5); // Balairung UGM -> GSP
+    addEdge(mat, 3, 4, 1.9); // Balairung UGM -> Faculty of MIPA
 
-    addEdge(mat, 4, 1, 2.1);
-    addEdge(mat, 4, 2, 1.5);
-    addEdge(mat, 4, 3, 1.9);
+    addEdge(mat, 4, 1, 2.1); // Faculty of MIPA -> GSP
+    addEdge(mat, 4, 2, 1.5); // Faculty of MIPA -> RSGM
+    addEdge(mat, 4, 3, 1.9); // Faculty of MIPA -> Balairung UGM
 
     displayMatrix(mat);
 
